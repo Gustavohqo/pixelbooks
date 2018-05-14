cc6f7e97dae55fdfea74b15927acfe3b792ebf21
Gustavo Oliveira <gustavo.oliveira@splab.ufcg.edu.br>
2018-05-14 14:53:43 -0300

    criação de rota de teste

--- src/main/java/br/com/pixelbooks/app/PixelbooksApplication.java
+++ src/main/java/br/com/pixelbooks/app/PixelbooksApplication.java
@@ -1,9 +1,9 @@
-package br.com.pixelbooks.pixelbooks;
+package br.com.pixelbooks.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.pixelbooks.app")
 public class PixelbooksApplication {
 
 	public static void main(String[] args) {
--- src/test/java/br/com/pixelbooks/app/PixelbooksApplicationTests.java
+++ src/test/java/br/com/pixelbooks/app/PixelbooksApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.pixelbooks.pixelbooks;
+package br.com.pixelbooks.app;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
